d2bca021bd79d337bf308436ca9ddf6368d1bcc9
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-06-24 09:05:10 +0000

    Added form display toggle

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const myLibrary = []
+const addButton = document.querySelector(".add")
+const bookForm = document.querySelector(".bookForm")
+const submitForm = document.querySelector(".submit")
+
+addButton.addEventListener("click",()=>{
+    bookForm.style.display = "flex"
+})
+
+submitForm.addEventListener("click",()=>{
+    bookForm.style.display = "none"
+})
